fa7aa8db8ebcd297ae503a726132ec3d04731e41
Alexis Tyler <xo@wvvw.me>
2017-04-13 11:54:21 +0930

    fix args passing too many params

--- src/index.js
+++ src/index.js
@@ -59,10 +59,10 @@ class Cz {
         fs.writeFileSync(path.normalize(newPath || this._path), JSON.stringify(this._config, null, 4) + '\n', 'utf8');
     }
 
-    args(save) {
+    args() {
         process.argv.slice(2).forEach(function(val) {
             val = val.slice(2).split('='); // Gets us ['db:host', 'localhost'] from '--db:host=localhost'
-            this.set(val[0], val[1], save);
+            this.set(val[0], val[1]);
         });
     }
 
